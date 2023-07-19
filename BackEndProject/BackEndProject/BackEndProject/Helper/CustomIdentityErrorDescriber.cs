a7c8514a8f7cd87a318eeffe877cdc3612aad3d4
alizadaln <lalana@code.edu.az>
2023-07-19 20:18:02 +0400

    CorrectionProject

---
+++ BackEndProject/BackEndProject/BackEndProject/Helper/CustomIdentityErrorDescriber.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BackEndProject.Helper
+{
+    public class CustomIdentityErrorDescriber : IdentityErrorDescriber
+    {
+        public override IdentityError PasswordTooShort(int length) { return new IdentityError { Code = nameof(PasswordTooShort), Description = $"Shifre 8 simvoldan boyuk olmalidir." }; }
+
+        public override IdentityError PasswordRequiresNonAlphanumeric() { return new IdentityError { Code = nameof(PasswordRequiresNonAlphanumeric), Description = "Shifrede simvol olmalidir." }; }
+        public override IdentityError PasswordRequiresDigit() { return new IdentityError { Code = nameof(PasswordRequiresDigit), Description = "Shifrede minimum 1 reqem olmalidir." }; }
+        public override IdentityError PasswordRequiresLower() { return new IdentityError { Code = nameof(PasswordRequiresLower), Description = "Shifrede minimum 1 kichik herf olmalidir." }; }
+        public override IdentityError PasswordRequiresUpper() { return new IdentityError { Code = nameof(PasswordRequiresUpper), Description = "Shifrede minimum 1 boyuk herf olmalidir." }; }
+
+    }
+}
